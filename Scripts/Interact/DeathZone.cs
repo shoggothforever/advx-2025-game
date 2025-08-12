ab34ead89593f14611a397a056233b7807a65a39
shoggothforever <1337231450@qq.com>
2025-08-12 23:23:39 +0800

    fix death zone

--- Scripts/Interact/DeathZone.cs
+++ Scripts/Interact/DeathZone.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 using SaveYourself.Core;
 public class DeathZone : MonoBehaviour
 {
-    public void OnCollisionEnter2D(Collision2D c)
+    public void OnTriggerEnter2D(Collider2D c)
     {
+        Debug.Log("trigger death zone");
+        if (c.CompareTag("GhostPlayer"))
+        {
+            Debug.Log("you die ");
+            LoaderManager.Instance.LoadScene(GameManager.Instance.levelName);
+        }
+        else if (c.CompareTag("Player"))
+        {
+            GameManager.Instance.RestartFromPreForward();
+        }
+    }
+    public void OnCollision2D(Collision2D c)
+    {
+        Debug.Log("collision death zone");
         if (c.collider.CompareTag("GhostPlayer"))
         {
+            Debug.Log("you die ");
             LoaderManager.Instance.LoadScene(GameManager.Instance.levelName);
         }
         else if (c.collider.CompareTag("Player"))
